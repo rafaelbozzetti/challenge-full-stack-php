3c1861c18497a1869036018c9db0400ee0538191
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-27 20:13:06 -0300

    chore: add HomeActionController, change App namespace

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,7 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', function (
-        ServerRequestInterface $request,
-        ResponseInterface $response
-    ) {
-        $response->getBody()->write('Hello, World!');
-
-        return $response;
-    });
+    $app->get('/', \App\Action\HomeAction::class);
 };
