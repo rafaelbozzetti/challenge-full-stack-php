ef679fbeaa73105d1b02500ae688d98027da914f
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-27 22:01:28 -0300

    chore: add phinx library and create a database migrations

---
+++ db/migrations/20200728005908_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateUsersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up(): void
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string')
+              ->addColumn('password', 'string')
+              ->create();
+    }
+
+    public function down(): void
+    {
+        $this->table('users')->drop()->save();
+    }
+}
