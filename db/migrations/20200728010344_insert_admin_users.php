6714952e0471310a281188528807cbc50448d905
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-27 22:07:38 -0300

    chore: create a migration to add a admin users on database

---
+++ db/migrations/20200728010344_insert_admin_users.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class InsertAdminUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $password = sha1('admin');
+        $builder = $this->getQueryBuilder();
+        $builder
+            ->insert(['email', 'password'])
+            ->into('users')
+            ->values(['email' => 'admin@grupoa.com.br', 'password' => "$password" ])
+            ->execute();
+    }
+}
