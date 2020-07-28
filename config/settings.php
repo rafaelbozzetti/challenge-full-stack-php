b6a857d7860639a35409e9373dee7a6373b9ce6c
Rafael Bozzetti <rafaelbozzetti@gmail.com>
2020-07-28 08:19:07 -0300

    chore: database configuration

--- config/settings.php
+++ config/settings.php
@@ -15,41 +15,6 @@ $settings['root'] = dirname(__DIR__);
 $settings['temp'] = $settings['root'] . '/tmp';
 $settings['public'] = $settings['root'] . '/public';
 
-$settings['twig'] = [
-    // Template paths
-    'paths' => [
-        __DIR__ . '/../templates',
-    ],
-    // Twig environment options
-    'options' => [
-        // Should be set to true in production
-        'cache_enabled' => false,
-        'cache_path' => __DIR__ . '/../tmp/twig',
-    ],
-];
-
-$settings['db'] = [
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'username' => 'root',
-    'database' => 'grupoa',
-    'password' => 'sawa',
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'flags' => [
-        // Turn off persistent connections
-        PDO::ATTR_PERSISTENT => false,
-        // Enable exceptions
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // Emulate prepared statements
-        PDO::ATTR_EMULATE_PREPARES => true,
-        // Set default fetch mode to array
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        // Set character set
-        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
-    ],
-];
-
 // Error Handling Middleware settings
 $settings['error'] = [
 
@@ -63,6 +28,45 @@ $settings['error'] = [
 
     // Display error details in error log
     'log_error_details' => true,
+];
+
+$settings['twig'] = [
+    // Template paths
+    'paths' => [
+        __DIR__ . '/../templates',
+    ],
+    // Twig environment options
+    'options' => [
+        // Should be set to true in production
+        'cache_enabled' => false,
+        'cache_path' => __DIR__ . '/../tmp/twig',
+    ],
+];
+
+$settings['session'] = [
+    'name' => 'webapp',
+    'cache_expire' => 0,
+];
+
+$settings['db'] = [
+    'driver' => \Cake\Database\Driver\Mysql::class,
+    'host' => 'localhost',
+    'database' => 'grupoa',
+    'username' => 'rafael',    
+    'password' => 'sawa',
+    'encoding' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'quoteIdentifiers' => true,
+    'timezone' => null,
+    'cacheMetadata' => false,
+    'log' => false,
+    'flags' => [
+        PDO::ATTR_PERSISTENT => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => true,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ],
 ];
 
 return $settings;
